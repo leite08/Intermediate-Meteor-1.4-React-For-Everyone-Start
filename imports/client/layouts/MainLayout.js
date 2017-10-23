0fd4475305ce3b9c2a0b6e16f11054439c9ad272
Rafael Leite <leite08@gmail.com>
2017-10-23 14:34:24 -0200

    Small fix on app title

--- imports/client/layouts/MainLayout.js
+++ imports/client/layouts/MainLayout.js
@@ -6,7 +6,7 @@ const MainLayout = (props) => {
   return (
     <div className="main-layout">
       <header>
-        <h1><Link to='/'>Level Up Voting</Link></h1>
+        <h1><Link to='/'>Level Up Voting!</Link></h1>
         <LoginButtons />
         <nav>
           <Link to='/about'>About</Link>
